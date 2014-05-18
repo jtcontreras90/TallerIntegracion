810d531267daf2d80be0cafc6d61e21c90c63a67
Julio Adriazola <jcadriaz@uc.cl>
2014-05-17 23:51:33 -0400

    orden de pricing

--- app/models/pricing.rb
+++ app/models/pricing.rb
@@ -1,4 +1,18 @@
 class Pricing < ActiveRecord::Base
+	
+	 def self.findBySKU(sku)
+	 	puts Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now).inspect
+	 	return Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now).inspect
+	 end
 
+	 def self.findById(id)
+	 	puts Pricing.where("id_pricing = ? and fecha_actualizacion <= ? and fecha_vigencia > ?",id,DateTime.now,DateTime.now).inspect
+	 	return Pricing.where("id_pricing = ? and fecha_actualizacion <= ? and fecha_vigencia > ?",id,DateTime.now,DateTime.now).inspect
+	 end
+
+	 def self.precioBySKU(sku)
+	 	puts Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now)[0].precio
+			return Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now)
+	 end
 
 end
