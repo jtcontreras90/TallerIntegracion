ab76e91c5043555c51fb9369a3610505fadb3626
jsrojas1 <jsrojas1@uc.cl>
2014-05-17 22:16:36 -0400

    queries sobre la base de datos de pricing

--- app/models/database.rb
+++ app/models/database.rb
@@ -125,6 +125,23 @@ class Database
 		end		
 
     end
+
+    def self.findBySKU(sku)
+
+    	puts Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now).inspect
+    	return Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now).inspect
+    end
+
+    def self.findById(id)
+    	puts Pricing.where("id_pricing = ? and fecha_actualizacion <= ? and fecha_vigencia > ?",id,DateTime.now,DateTime.now).inspect
+    	return Pricing.where("id_pricing = ? and fecha_actualizacion <= ? and fecha_vigencia > ?",id,DateTime.now,DateTime.now).inspect
+    end
+
+    def self.precioBySKU(sku)
+    	puts Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now)[0].precio
+   		return Pricing.where("sku like ? and fecha_actualizacion <= ? and fecha_vigencia > ?","%#{sku}%",DateTime.now,DateTime.now)
+    end
+
 end
 
 
