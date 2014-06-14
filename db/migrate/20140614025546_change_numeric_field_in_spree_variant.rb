9374fb9f97189945e2ea7387d4e79136b3db7257
marrano_ <marrano_@marrano-R480.(none)>
2014-06-13 23:08:24 -0400

    ...

--- db/migrate/20140614025546_change_numeric_field_in_spree_variant.rb
+++ db/migrate/20140614025546_change_numeric_field_in_spree_variant.rb
@@ -1,5 +1,5 @@
 class ChangeNumericFieldInSpreeVariant < ActiveRecord::Migration
   def change
-  	change_column :spree_variant, :cost_price, :decimal, :precision => 14, :scale => 2
+  	change_column :spree_variants, :cost_price, :decimal, :precision => 14, :scale => 2
   end
 end
