0c6ee65ef8232b1c9927bdbf61f10a5b810abc61
marrano_ <marrano_@marrano-R480.(none)>
2014-06-18 21:59:33 -0400

    Stock

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,11 +1,26 @@
 Spree::Product.class_eval do
   def refreshAttributes
-	  	if rand(0..1)==1
-	  		self.price=rand(0..100000000)
-	  	else
-	  		self.price=self.internet_price
-	  	end
-	  	self.save
+  	if rand(0..1)==1
+  		self.price=rand(0..100000000)
+  	else
+  		self.price=self.internet_price
+  	end
+  	self.save
+  end
+
+  def refreshStock
+	si=self.stock_items.first
+	sku=self.sku
+	stock=0
+	begin
+		stock=ApiBodega.obtenerStock sku
+	rescue
+	end
+	puts "-------------------------------------------------"
+	puts "stock: #{stock}"
+	si['count_on_hand']=stock
+
+	si.save
   end
 
 end
