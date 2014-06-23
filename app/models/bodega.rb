ff96588e7e78dac5590041a5aae439213b70c656
jtcontreras90 <jtcontr1@uc.cl>
2014-06-22 23:04:28 -0400

    agregada la integración con los grupos 1 y 3. Agregados los esqueletos de los grupos 2 6 y 7

--- app/models/bodega.rb
+++ app/models/bodega.rb
@@ -24,8 +24,44 @@
       if contar < cantidad #Acá a penas se recibe algo se acepta, no nos aseguramos de recibir TODO el pedido
         case i
         when 1
+          url="http://integra1.ing.puc.cl/ecommerce/api/v1/pedirProducto"
+          user="grupo9"
+          pass="QWhiPGn2Hnm54"
+          almacenId="53571e54682f95b80b786eb9"
+          begin
+            response=RestClient.post url,{:usuario=>user, :password=>pass, :almacenId=>almacenId, :sku=>sku, :cant=>cantidad-contar }
+            if not JSON.parse(response.body).has_key?('error')
+              contar=contar+JSON.parse(response.body)[:amountSent]
+            end
+          rescue
+            Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
+          end        
         when 2
+          # url="http://integra2.ing.puc.cl/api/pedirProducto"
+          # user="grupo9"
+          # pass="QWhiPGn2Hnm54"
+          # almacenId="53571e54682f95b80b786eb9"
+          # begin
+            # response=RestClient.post url,{:usuario=>user, :password=>pass, :almacen_id=>almacenId, :SKU=>sku, :cantidad=>cantidad-contar }
+            # if not JSON.parse(response.body).has_key?('error')
+              # cantidad=0
+            # end
+          # rescue
+            # Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
+          # end 
         when 3
+          url="http://integra3.ing.puc.cl/api/pedirProducto"
+          user="grupo9"
+          pass="grupo9" #sha1
+          almacenId="53571e54682f95b80b786eb9"
+          begin
+            response=RestClient.post url,{:usuario=>user, :password=>pass, :almacen_id=>almacenId, :SKU=>sku, :cantidad=>cantidad-contar }
+            if not JSON.parse(response.body).has_key?('error')
+              contar=contar+JSON.parse(response.body)[:cantidad]
+            end
+          rescue
+            Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
+          end
         when 4 #NO FUNCIONA (error: "Nombre de usuario o password invalida")
           url="http://integra4.ing.puc.cl/api/pedirProducto"
           user="grupo9"
@@ -34,7 +70,7 @@
           begin
             response=RestClient.post url,{:usuario=>user, :password=>pass, :almacen_id=>almacenId, :SKU=>sku, :cantidad=>cantidad-contar }
             if not JSON.parse(response.body).has_key?('error')
-              cantidad=0
+              contar=contar+JSON.parse(response.body)[:cantidad]
             end
           rescue
             Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
@@ -53,7 +89,31 @@
             Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
           end
         when 6
+          # url="http://integra6.ing.puc.cl/apiGrupo/pedido"
+          # user="grupo9"
+          # pass="795f5a03cad01447898fb5861de0d0af6115b0c1" #sha1
+          # almacenId="53571e54682f95b80b786eb9"
+          # begin
+            # response=RestClient.post url,{:usuario=>user, :password=>pass, :almacen_id=>almacenId, :SKU=>sku, :cantidad=>cantidad-contar }
+            # if not JSON.parse(response.body).has_key?('error')
+              # cantidad=0
+            # end
+          # rescue
+            # Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
+          # end
         when 7
+          # url="http:/integra7.ing.puc.cl/api/api_request"
+          # user="grupo9"
+          # pass="JgS9I4od03" #pass
+          # almacenId="53571e54682f95b80b786eb9" 
+          # begin
+            # response=RestClient.post url,{:usuario=>user, :password=>pass, :almacenId=>almacenId, :sku=>sku, :cantidad=>cantidad-contar }
+            # if not JSON.parse(response.body).has_key?('error')
+              # contar=contar+JSON.parse(response.body)[:cantidad]
+            # end
+          # rescue
+            # Rails.logger.info "[SCHEDULE][BODEGA.PEDIRPRODUCTO]Error in conecction with group #{i}"
+          # end
         when 8 #FUNCIONA
           url="http://integra8.ing.puc.cl/api/pedirProducto"
           user="grupo9"
