5b7895a7560ac5df960fa7ae370927eb2640621d
marrano_ <marrano_@marrano-R480.(none)>
2014-06-26 23:00:54 -0400

    precio de producto y stock OK

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,10 +1,9 @@
 Spree::Product.class_eval do
   def refreshAttributes
-  	if rand(0..1)==1
-  		self.price=rand(0..100000000)
-  	else
-  		self.price=self.internet_price
-  	end
+  	puts "-------------------------------------------------"
+  	minPrice=[self.validInternetPrice, self.precioOferta, self.precioPricing].min
+  	puts "precio: #{minPrice}"
+  	self.price=minPrice
   	self.save
   end
 
@@ -21,6 +20,40 @@ Spree::Product.class_eval do
 	si['count_on_hand']=stock
 
 	si.save
+  end
+
+  def precioOferta
+  	p=1000000000
+  	ofertas=Oferta.where('sku = ? AND fecha_inicio < ? AND fecha_termino > ?', self.sku, DateTime.now, DateTime.now).order('precio asc')
+  	if ofertas.count > 0
+  		p=ofertas[0].precio
+  	end
+  	p
+  end
+
+  def precioPricing
+  	p=1000000000
+  	#Si el precio es 0 se considera como válido considerar el precio Pricing
+  	if self.internet_price==0
+  		pricing=Pricing.findBySKU(self.sku)
+	  	if pricing
+	  		p=pricing.precio
+	  	end
+  	end
+  	p
+  end
+
+  def tieneOferta
+  	Oferta.where('sku = ? AND fecha_inicio < ? AND fecha_termino > ?', self.sku, DateTime.now, DateTime.now).count > 0
+  end
+
+  def validInternetPrice
+  	p=1000000000
+  	#Si el precio es 0 se considera inválido y se obtiene el precio por otro medio
+  	if self.internet_price>0
+  		p=self.internet_price
+  	end
+  	p
   end
 
 end
