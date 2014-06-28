9501fcc03535c0f75b912a69c4b5c5f22861fc3e
marrano_ <marrano_@marrano-R480.(none)>
2014-06-28 01:21:13 -0400

    cosas

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,37 +3,40 @@ class MapController < ApplicationController
 		pedidoID=nil
 		@paquetesPedidos=[]
 		@h=Pedido.all.map{|x| x.pedidoID}.uniq.count
-		Pedido.order('created_at desc').map{|x| x.pedidoID}.uniq[params[:offset].to_i,10].each do |p|
-			a={}
-			b=[]
-			parcial = false
-			Pedido.where(pedidoID: p).each_with_index do |pedido,i|
-				if i==0
-					a['color']="green"
-					a['nombreCliente']=pedido.rut
-					a['direccion']=pedido.direccion
-					
-				end
-				c={}
-				c['cantidadSolicitada']=pedido.cantidad
-				c['cantidadEnviada']=pedido.cant_vendida
-				c['sku']= pedido.sku
-				c['quiebre']=pedido.quebrado
-				b<<c
-				if pedido.quebrado
-					a['color']="red"
-				else
-					parcial = true
-				end
+		pedidos=Pedido.order('created_at desc').map{|x| x.pedidoID}.uniq[params[:offset].to_i,10]
+		if pedidos
+			pedidos.each do |p|
+				a={}
+				b=[]
+				parcial = false
+				Pedido.where(pedidoID: p).each_with_index do |pedido,i|
+					if i==0
+						a['color']="green"
+						a['nombreCliente']=pedido.rut
+						a['direccion']=pedido.direccion
+						
+					end
+					c={}
+					c['cantidadSolicitada']=pedido.cantidad
+					c['cantidadEnviada']=pedido.cant_vendida
+					c['sku']= pedido.sku
+					c['quiebre']=pedido.quebrado
+					b<<c
+					if pedido.quebrado
+						a['color']="red"
+					else
+						parcial = true
+					end
 
-				if pedido.cantidad > pedido.cant_vendida
-					a['color']="yellow"
+					if pedido.cantidad > pedido.cant_vendida
+						a['color']="yellow"
+
+					end
 
 				end
-
+				a['pedido']=b
+				@paquetesPedidos<<a
 			end
-			a['pedido']=b
-			@paquetesPedidos<<a
 		end
 		@paquetesPedidos
 	end
